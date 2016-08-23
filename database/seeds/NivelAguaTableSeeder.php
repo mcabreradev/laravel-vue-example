e12797c9c005c06def6c79be7003fe624c304f02
Leonel Viera <vieraleonel1@gmail.com>
2016-08-23 11:12:30 -0300

    Nuevos Seeds para nivel de agua

--- database/seeds/NivelAguaTableSeeder.php
+++ database/seeds/NivelAguaTableSeeder.php
@@ -13,19 +13,19 @@ class NivelAguaTableSeeder extends Seeder
     public function run()
     {
         NivelAgua::create([
-            'titulo'      => 'Menos del 15%',
+            'titulo'      => 'Menos del 20%',
             'color'       => '#CA3A27',
             'descripcion' => 'Se ruega optimizar al máximo el consumo'
         ]);
 
         NivelAgua::create([
-            'titulo'      => 'Menos del 25%',
+            'titulo'      => 'Menos del 40%',
             'color'       => '#FF8000',
             'descripcion' => 'Se ruega optimizar el consumo. Baja presión generalizada'
         ]);
 
         NivelAgua::create([
-            'titulo'      => 'Menos del 50%',
+            'titulo'      => 'Menos del 60%',
             'color'       => '#D9D900',
             'descripcion' => 'Servicio funcionando normalmente'
         ]);
