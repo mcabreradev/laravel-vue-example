1373c6f033f60074390e03d5f4f8cc0c40bfe747
Leonel Viera <vieraleonel1@gmail.com>
2016-08-23 14:46:58 -0300

    Corrección al modelo de usuarios del sistema

--- app/Models/Admin/User.php
+++ app/Models/Admin/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'fisrtname', 'lastname', 'email', 'password',
+        'username', 'firstname', 'lastname', 'email', 'password',
     ];
 
     /**
