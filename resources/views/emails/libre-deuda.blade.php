c5afd9652655587a8fd141439386373ca48735a7
Leonel Viera <vieraleonel1@gmail.com>
2016-09-08 17:59:56 -0300

    Mejoras a la plantilla de email de libre deuda

--- resources/views/emails/libre-deuda.blade.php
+++ resources/views/emails/libre-deuda.blade.php
@@ -5,21 +5,21 @@
 </head>
 <body>
 
-  <h3>Solicitante<h3>
-  <p><b>Apellido</b> {{ $data['solicitante_apellido'] }}</p>
-  <p><b>Nombre</b> {{ $data['solicitante_nombre'] }}</p>
-  <p><b>Teléfono</b> {{ $data['solicitante_telefono'] }}</p>
-  <p><b>Correo</b> {{ $data['solicitante_email'] }}</p>
+  <h1>Solicitante</h1>
+  <p><b>Apellido:</b> {{ $data['solicitante_apellido'] ?: '' }}</p>
+  <p><b>Nombre:</b> {{ $data['solicitante_nombre'] ?: '' }}</p>
+  <p><b>Teléfono:</b> {{ $data['solicitante_telefono'] ?: '' }}</p>
+  <p><b>Correo:</b> {{ $data['solicitante_email'] ?: '' }}</p>
 
-  <h3>Titular</h3>
-  <p><b>Nombre</b> {{ $data['titular_nombre'] }}</p>
-  <p><b>Documento</b> {{ $data['titular_documento'] }}</p>
-  <p><b>Domicilio</b> {{ $data['domicilio'] }}</p>
-  <p><b>Expediente</b> {{ $data['expediente'] }}</p>
-  <p><b>Unidad</b> {{ $data['unidad'] }}</p>
+  <h1>Titular</h1>
+  <p><b>Nombre:</b> {{ $data['titular_nombre'] ?: '' }}</p>
+  <p><b>Documento:</b> {{ $data['titular_documento'] ?: '' }}</p>
+  <p><b>Domicilio:</b> {{ $data['domicilio'] ?: '' }}</p>
+  <p><b>Expediente:</b> {{ $data['expediente'] ?: '' }}</p>
+  <p><b>Unidad:</b> {{ $data['unidad'] ?: '' }}</p>
 
-  <h3>Descripción</h3>
-  <p>{{ $data['descripcion'] }}</p>
+  <h1>Descripción</h1>
+  <p>{{ $data['descripcion'] ?: '' }}</p>
 
 </body>
 </html>
