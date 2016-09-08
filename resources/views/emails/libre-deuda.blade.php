8547d9530b529d703949dceb5ac9c839caa6647b
Leonel Viera <vieraleonel1@gmail.com>
2016-09-08 17:54:06 -0300

    Pequeños cambios para probar SMTP

--- resources/views/emails/libre-deuda.blade.php
+++ resources/views/emails/libre-deuda.blade.php
@@ -5,29 +5,21 @@
 </head>
 <body>
 
-  <table>
-    <tbody>
-      <tr>
-        <th colspan="2"><p>Solicitante</p></th>
-      </tr>
-      <tr>
-        <th><p>Apellido</p></th>
-        <td><p>{{ $data['solicitante_apellido'] }}</p>
-      </tr>
-      <tr>
-        <th><p>Nombre</p></th>
-        <td><p>{{ $data['solicitante_nombre'] }}</p>
-      </tr>
-      <tr>
-        <th><p>Teléfono</p></th>
-        <td><p>{{ $data['solicitante_telefono'] }}</p>
-      </tr>
-      <tr>
-        <th><p>Correo electrónico</p></th>
-        <td><p>{{ $data['solicitante_email'] }}</p>
-      </tr>
-    </tbody>
-  </table>
+  <h3>Solicitante<h3>
+  <p><b>Apellido</b> {{ $data['solicitante_apellido'] }}</p>
+  <p><b>Nombre</b> {{ $data['solicitante_nombre'] }}</p>
+  <p><b>Teléfono</b> {{ $data['solicitante_telefono'] }}</p>
+  <p><b>Correo</b> {{ $data['solicitante_email'] }}</p>
+
+  <h3>Titular</h3>
+  <p><b>Nombre</b> {{ $data['titular_nombre'] }}</p>
+  <p><b>Documento</b> {{ $data['titular_documento'] }}</p>
+  <p><b>Domicilio</b> {{ $data['domicilio'] }}</p>
+  <p><b>Expediente</b> {{ $data['expediente'] }}</p>
+  <p><b>Unidad</b> {{ $data['unidad'] }}</p>
+
+  <h3>Descripción</h3>
+  <p>{{ $data['descripcion'] }}</p>
 
 </body>
 </html>
