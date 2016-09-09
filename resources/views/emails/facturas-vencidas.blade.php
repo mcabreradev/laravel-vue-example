02c4297485e1af68d3caaf8aeb2ba520e709dc34
Leonel Viera <vieraleonel1@gmail.com>
2016-09-09 05:56:48 -0300

    Incluyo título al inicio del cuerpo del email explicando el tipo de solicitud

--- resources/views/emails/facturas-vencidas.blade.php
+++ resources/views/emails/facturas-vencidas.blade.php
@@ -5,13 +5,15 @@
 </head>
 <body>
 
-  <h1>Solicitante</h1>
+  <h1>Solicitud de facturas vencidas</h1>
+
+  <h2>Solicitante</h2>
   <p><b>Apellido:</b> {{ $data['solicitante_apellido'] ?: '' }}</p>
   <p><b>Nombre:</b> {{ $data['solicitante_nombre'] ?: '' }}</p>
   <p><b>Teléfono:</b> {{ $data['solicitante_telefono'] ?: '' }}</p>
   <p><b>Correo:</b> {{ $data['solicitante_email'] ?: '' }}</p>
 
-  <h1>Factura</h1>
+  <h2>Factura</h2>
   <p><b>Domicilio:</b> {{ $data['domicilio'] ?: '' }}</p>
   <p><b>Localidad:</b> {{ $data['localidad'] ?: '' }}</p>
   <p><b>Nomenclatura:</b> {{ $data['nomenclatura'] ?: '' }}</p>
@@ -19,7 +21,7 @@
   <p><b>Períodos:</b> {{ $data['periodo'] ?: '' }}</p>
   <p><b>Entrega:</b> {{ $data['entrega'] ?: '' }}</p>
 
-  <h1>Comentarios</h1>
+  <h2>Comentarios</h2>
   <p>{{ $data['comentarios'] ?: '' }}</p>
 
 </body>
--- resources/views/emails/libre-deuda.blade.php
+++ resources/views/emails/libre-deuda.blade.php
@@ -5,23 +5,25 @@
 </head>
 <body>
 
-  <h1>Solicitante</h1>
+  <h1>Solicitud de libre deuda</h1>
+
+  <h2>Solicitante</h2>
   <p><b>Apellido:</b> {{ $data['solicitante_apellido'] ?: '' }}</p>
   <p><b>Nombre:</b> {{ $data['solicitante_nombre'] ?: '' }}</p>
   <p><b>Teléfono:</b> {{ $data['solicitante_telefono'] ?: '' }}</p>
   <p><b>Correo:</b> {{ $data['solicitante_email'] ?: '' }}</p>
 
-  <h1>Titular</h1>
+  <h2>Titular</h2>
   <p><b>Nombre:</b> {{ $data['titular_nombre'] ?: '' }}</p>
   <p><b>Documento:</b> {{ $data['titular_documento'] ?: '' }}</p>
 
-  <h1>Factura</h1>
+  <h2>Factura</h2>
   <p><b>Domicilio:</b> {{ $data['domicilio'] ?: '' }}</p>
   <p><b>Localidad:</b> {{ $data['localidad'] ?: '' }}</p>
   <p><b>Nomenclatura:</b> {{ $data['nomenclatura'] ?: '' }}</p>
   <p><b>Unidad:</b> {{ $data['unidad'] ?: '' }}</p>
 
-  <h1>Descripción</h1>
+  <h2>Descripción</h2>
   <p>{{ $data['descripcion'] ?: '' }}</p>
 
 </body>
