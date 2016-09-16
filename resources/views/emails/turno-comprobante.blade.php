61a1e037630cbc1c67a517f5deef86c05a5613ad
Leonel Viera <vieraleonel1@gmail.com>
2016-09-16 12:25:46 -0300

    Ajustes al template del email

--- resources/views/emails/turno-comprobante.blade.php
+++ resources/views/emails/turno-comprobante.blade.php
@@ -7,9 +7,9 @@
 
   <h1>Comprobante turno para {{ $turno->actividad->nombre }}</h1>
 
-  <h2>Te esperamos el {{ $turno->fecha->toDateString() }} a las {{ $turno->hora }}</h2>
+  <h2>Te esperamos el {{ $turno->fecha->format('d/m/Y') }} a las {{ $turno->hora }}</h2>
 
-  <h2>Solicitante</h2>
+  <h3>Solicitante</h3>
   <p><b>Apellido:</b> {{ $turno->usuario->apellido }}</p>
   <p><b>Nombre:</b> {{ $turno->usuario->nombre }}</p>
   <p><b>DNI/CUIT:</b> {{ $turno->usuario->documento }}</p>
