ab8b2fedde6a2858daa68371ca790ded4ad7057b
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2017-01-13 11:59:39 -0300

    se agrega un componente tootip con su debida documentacion

---
+++ resources/assets/js/components/Panal/Tooltip/index.js
@@ -0,0 +1,14 @@
+
+/**
+ *  Panal Tooltip
+ *
+ *  @title      String   titulo que lleva
+ *  @position   String   posicion del tooltip, por defecto es top, pero hay otros como left, right, bottom
+ *
+ *  <panal-tooltip
+ *      :title='Aqui el titulo'
+ *      :position='top'
+ *   </panal-tooltip>
+ */
+Vue.component('panal-tooltip', require('./tooltip.vue'));
+
---
+++ resources/assets/js/components/Panal/Tooltip/tooltip.vue
@@ -0,0 +1,23 @@
+<template>
+  <span role="button" data-toggle="tooltip" data-container="body" :data-placement="position" :title="title">
+    <slot></slot>
+  </span>
+</template>
+<script>
+  export default {
+    name: 'panal-tooltip',
+    props: {
+      title: {
+        type: String,
+        default: () => '',
+        required: true
+      },
+      position: {
+        type: String,
+        default: () => 'top',
+        required: false
+      },
+    }
+  }
+
+</script>
