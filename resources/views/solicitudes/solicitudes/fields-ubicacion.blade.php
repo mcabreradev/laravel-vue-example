2441f07fb0b64e28f957a4126a8f40ca3d099cc8
Viera Leonel <vieraleonel1@gmail.com>
2017-01-19 11:38:45 -0300

    Agrego lógica para ubicación en el mapa: - Se puede arrastrar un marcador que irá obteniendo actualizando las coordenadas - Se pueden completar los campos de latitud y longitud y estos actualizaran el marcador en el mapa - Se pueden solicitar inferir las coordenadas a partir de la calle

--- resources/views/solicitudes/solicitudes/fields-ubicacion.blade.php
+++ resources/views/solicitudes/solicitudes/fields-ubicacion.blade.php
@@ -28,20 +28,17 @@
       <input class="form-control" type="text" name="lugar_entre_2" id="lugar_entre_2" value="{{ old('lugar_entre_2', $solicitud->lugar_entre_2) }}">
     </div>
 
+    <div class="form-group">
+      <button id="inferir-coordenadas-btn" type="button" class="btn btn-primary" data-loading-text="Infiriendo...">
+        <span class="fa fa-map-marker"></span> Inferir coordenadas
+      </button>
+    </div>
+
   </div>
   <div class="col-md-6 col-xs-12">
 
-    <div class="form-group">
-    <!--  <panal-map
-       height="300px"
-       :zoom="{{$solicitud->id ? 10 : 8}}"
-       :lat="{{ old('lat', $solicitud->lat) }}"
-       :lng="{{ old('lng', $solicitud->lng) }}">
-     </panal-map> -->
-    </div>
-
     <div class="form-group text-center">
-      <i class="fa fa-picture-o" aria-hidden="true" style="font-size: 15.1em;"></i>
+      <div id="mapa-ubicacion" style="width: 100%; height: 300px"></div>
     </div>
 
     <div class="form-group">
@@ -56,3 +53,124 @@
 
   </div>
 </div>
+
+@section('body-scripts')
+  @parent
+
+  @include('common.maps')
+  <script>
+    (function($, L){
+      'use strict';
+
+      // inicializo variables
+      var $lat     = $('#lat'),
+          $lng     = $('#lng'),
+          lat      = $lat.val(),
+          lng      = $lng.val(),
+          map      = null,
+          marker   = null,
+          baseMaps = {},
+          geocoder = new google.maps.Geocoder();
+
+      // ajusto valores de lat y lng tanto en varaibles como en inputs
+      if (lat == '') {
+        lat = -54.8033601;
+        $lat.val(lat);
+      }
+
+      if (lng == '') {
+        lng = -68.3172124;
+        $lng.val(lng);
+      }
+
+      // creo mapa centrado en Ushuaia
+      map = new L.Map('mapa-ubicacion', {scrollWheelZoom: false})
+        .setView([lat, lng], 15);
+
+      // agrego marker que servira para ajustar las coordenadas
+      marker = L.marker([lat, lng], {draggable:'true'});
+      marker.addTo(map);
+
+      // evento para actualizar las coordenadas al arrastrarse el marker
+      marker.on('dragend', function(event){
+        var draggedMarker = event.target;
+        var position      = draggedMarker.getLatLng();
+        var latlng        = new L.LatLng(position.lat, position.lng);
+
+        draggedMarker.setLatLng(latlng,{draggable:'true'});
+        map.panTo(latlng)
+
+        $lat.val(position.lat);
+        $lng.val(position.lng);
+      });
+
+      // eventos para actualizarce el marker al completar los inputs
+      $('#lat, #lng').on('change', function(){
+        var latlng = new L.LatLng($lat.val(), $lng.val());
+
+        marker.setLatLng(latlng,{draggable:'true'});
+        map.panTo(latlng)
+      });
+
+      // capas disponibles
+      baseMaps = {
+        'Google carretera': new L.Google('ROADMAP'),
+        'Google satelital': new L.Google('HYBRID')
+      };
+      L.control.layers(baseMaps, {}).addTo(map);
+
+      // seteo la capa predeterminada
+      map.addLayer(baseMaps['Google carretera']);
+
+      // evento para inferir las coordenadas a partir de la calle
+      $('#inferir-coordenadas-btn').on('click', function(){
+        var $btn    = $(this),
+            address = '';
+
+        $btn.button('loading');
+
+        var ubicacionData = {
+          calle: $('#lugar_calle').val(),
+          numero: $('#lugar_numero').val(),
+          entre1: $('#lugar_entre_1').val(),
+          entre2: $('#lugar_entre_2').val()
+        };
+
+        // intento por calle + numero o por entre1 + entre2
+        if (ubicacionData.calle.length) {
+          address += ubicacionData.calle + ' ' + ubicacionData.numero;
+        }
+        else {
+          address += ubicacionData.entre1 + ' y ' + ubicacionData.entre2;
+        }
+
+        // siempre agrego la ciudad
+        address += ', Ushuaia, Tierra del Fuego, Argentina';
+
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+
+            // obtengo las posiciones
+            var latlng = new L.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+
+            // cambio el marker y centro el mapa
+            marker.setLatLng(latlng,{draggable:'true'});
+            map.panTo(latlng);
+
+            // actualizo los inputs
+            $lat.val(results[0].geometry.location.lat());
+            $lng.val(results[0].geometry.location.lng());
+
+            $btn.button('reset');
+
+          } else {
+            console.error('Geocode was not successful for the following reason: ' + status);
+            $btn.button('reset');
+          }
+        });
+      });
+
+    })(window.jQuery, window.L);
+  </script>
+
+@endsection
