b366a2a5ec7439c9258568e949ac2f93e00a01fb
Viera Leonel <vieraleonel1@gmail.com>
2017-01-31 12:55:02 -0300

    Borro ¿¿método de prueba ??

--- app/Models/AppModel.php
+++ app/Models/AppModel.php
@@ -7,26 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class AppModel extends Model
 {
-    /**
-    * Save the model and all of its relationships.
-    *
-    * @return bool
-    */
-    public function push2222() {
-        if ( ! $this->save()) return false;
 
-        // To sync all of the relationships to the database, we will simply spin through
-        // the relationships and save each model via this "push" method, which allows
-        // us to recurse into all of these nested relations for the model instance.
-
-        foreach ($this->relations as $models)
-        {
-            foreach (Collection::make($models) as $model)
-            {
-                if ( ! $model->push()) return false;
-            }
-        }
-
-        return true;
-    }
 }
